18e28fe9cd8aad527b271a8ee6a86e1b64fe1aab
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-04-11 22:12:46 +0700

    Create locals.tf

---
+++ src/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  vm_web_instance_name = "netology-develop-platform-${var.env}-${var.project}-${var.role}"
+  vm_db_instance_name = "netology-develop-platform-${var.env}-${var.project}-${var.role1}"
+
+}
