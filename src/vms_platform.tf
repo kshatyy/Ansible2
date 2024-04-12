67ad3864f29eafaa21bbbdd98ad55beec662d658
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-04-12 09:41:04 +0700

    Update vms_platform.tf

--- src/vms_platform.tf
+++ src/vms_platform.tf
@@ -1,100 +1,40 @@
-variable "vm_web_image" {
-  type        = string
-  default     = "ubuntu-2004-lts"
-  description = "image_yandex_cloud"
-}
-
-variable "vm_web_name" {
-  type = string
-  default = "netology-develop-platform-web"
-  description = "Name_VM"
-}
-
-variable "vm_web_platform" {
-  type = string
-  default = "standard-v1"
-  description = "platform_id"
-}
-
-#variable "vm_web_core" {
-  #type = string
-  #default = "2"
-  #description = "CPU"
-#}
-
-#variable "vm_web_memory" {
-  #type = string
-  #default = "1"
-  #description = "RAM"
-#}
-
-#variable "vm_web_core_fraction" {
-  #type = string
-  #default = "5"
-  #description = "core_fraction"
-#}
-
-variable "vm_db_image" {
-  type = string
-  default = "ubuntu-2004-lts"
-}
-
-variable "vm_db_name" {
-  type = string
-  default = "netology-develop-platform-db"
-}
-
-variable "vm_db_platform" {
-  type = string
-  default = "standard-v1"
-}
-
-#variable "vm_db_core" {
-  #type = string
-  #default = "2"
-#}
-
-#variable "vm_db_memory" {
-  #type = string
-  #default = "2"
-#}
-
-#variable "vm_db_fraction" {
-  #type = string
-  #default = "20"
-#}
-
-
-variable "vm_db_zone_b" {
-  type        = string
-  default     = "ru-central1-b"
-}
-
-variable "vm_db_vpc_name" {
-  type        = string
-  default     = "develop1"
-  description = "VPC network & subnet name"
-}
-
-variable "vm_db_default_cidr" {
-  type        = list(string)
-  default     = ["10.1.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
-}
-
-
-variable "vm_resource" {
-  description = "ALL Vm's"
-  type = map(map(number))
+variable "vm_web_" {
+  type = object({
+    platform_id  = string
+    zone         = string
+    scheduling_policy = object({
+      preemptible = bool
+    })
+    network_interface = object({
+      nat = bool
+    })
+  })
   default = {
-    vm_web_resources = {
+    platform_id  = "standard-v1"
+    zone         = "ru-central1-a"
+    scheduling_policy = {
+      preemptible = true
+    }
+     network_interface = {
+       nat = true
+     }
+  }
+  description = "Configuration WEB"
+}
+
+variable "vms_resources" {
+  type = map(object({
+    cores = number
+    memory = number
+    core_fraction = number
+  }))
+  default = {
+    web = {
       cores = 2
       memory = 1
       core_fraction = 5
     }
-
-
-    vm_db_resources = {
+    db = {
       cores = 2
       memory = 2
       core_fraction = 20
@@ -102,11 +42,100 @@ variable "vm_resource" {
   }
 }
 
-variable "metadata_ssh" {
-  description = "metadata for all vms"
-  type        = map(string)
-  default     = {
-    serial-port-enable = "1"
-    ssh-keys          = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6+gW2b2YC/Rm4ejiaa/+RH8SZ2Eb6LSQzBVtDLWExm kshatyy@ya.ru"
+variable "metadata" {
+  type = map
+  default = {
+    serial-port-enable = 1
+    ssh-keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFt73GFkm57sjtP"
+  }
+}
+
+variable "project" {
+  type        = string
+  default     = "netology-develop-platform"
+  description = "Name project"
+}
+
+variable "instance" {
+  type = object({
+    db   = string
+    web  = string
+  })
+  default = {
+    db   = "db"
+    web  = "web"
+  }
+  description = "Instances names"
+}
+
+variable "each_vm" {
+  type = list(object({
+    instance_name             = string
+    instance_cores            = number
+    instance_memory           = number
+    instance_fraction         = number
+    instance_platform_id      = string
+    instance_zone             = string
+    instance_preemtable       = bool
+    instance_network_nat      = bool
+  }))
+  default = [
+    {
+      instance_name             = "main"
+      instance_cores            = 2
+      instance_memory           = 2
+      instance_fraction         = 5
+      instance_platform_id      = "standard-v1"
+      instance_zone             = "ru-central1-a"
+      instance_preemtable       = true
+      instance_network_nat      = true
+    },
+    {
+      instance_name             = "replica"
+      instance_cores            = 2
+      instance_memory           = 2
+      instance_fraction         = 5
+      instance_platform_id      = "standard-v1"
+      instance_zone             = "ru-central1-a"
+      instance_preemtable       = true
+      instance_network_nat      = true
+    }
+  ]
+}
+
+variable "storage" {
+  type = map(object({
+    instance_name             = string
+    instance_cores            = number
+    instance_memory           = number
+    instance_fraction         = number
+    instance_platform_id      = string
+    instance_zone             = string
+    instance_preemtable       = bool
+    instance_network_nat      = bool
+    disk = object({
+      disk_name     = string
+      disk_type     = string
+      disk_zone     = string
+      disk_size     = number
+    })
+  }))
+  default = {
+    vm = {
+      instance_name             = "storage"
+      instance_cores            = 2
+      instance_memory           = 2
+      instance_fraction         = 5
+      instance_platform_id      = "standard-v1"
+      instance_zone             = "ru-central1-a"
+      instance_preemtable       = true
+      instance_network_nat      = true
+      disk = {
+        disk_name     = "disk"
+        disk_type     = "network-hdd"
+        disk_zone     = "ru-central1-a"
+        disk_size     = 1
+      }
+    }
   }
 }
