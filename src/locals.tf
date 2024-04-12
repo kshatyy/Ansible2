478e07170290c9f2b9423f555d5dc87296c87c76
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-04-12 09:38:51 +0700

    Update locals.tf

--- src/locals.tf
+++ src/locals.tf
@@ -1,5 +1,3 @@
-locals {
-  vm_web_instance_name = "netology-develop-platform-${var.env}-${var.project}-${var.role}"
-  vm_db_instance_name = "netology-develop-platform-${var.env}-${var.project}-${var.role1}"
-
+ocals {
+  ssh_public_key = file("~/.ssh/id_ed25519.pub")
 }
