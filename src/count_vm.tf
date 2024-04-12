30e6570ae08767a6dfc30bb3f4d3ea66f3fb6f2a
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-04-12 09:51:18 +0700

    Create count_vm.tf

---
+++ src/count_vm.tf
@@ -0,0 +1,41 @@
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_web_yandex_compute_image
+}
+
+resource "yandex_compute_instance" "web" {
+  count       = 2
+  name        = "web-${count.index + 1}"
+  platform_id = var.vm_web_.platform_id
+
+  zone = var.vm_web_.zone
+
+  resources {
+    cores         = var.vms_resources.web.cores
+    memory        = var.vms_resources.web.memory
+    core_fraction = var.vms_resources.web.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_web_.scheduling_policy.preemptible
+  }
+
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = var.vm_web_.network_interface.nat
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = {
+    serial-port-enable = var.metadata.serial-port-enable
+    ssh-keys           = "ubuntu:${local.ssh_public_key}"
+  }
+
+  depends_on = [yandex_compute_instance.db]
+}
