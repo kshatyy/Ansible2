d4cd1aed63f597d985eaa4782af0ebc7b141969e
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-04-16 10:54:58 +0700

    Delete src directory

--- src/ansible.tf
+++
@@ -1,8 +0,0 @@
-resource "local_file" "hosts_cfg" {
-  content  = templatefile("${path.module}/hosts.tftpl", {
-    webservers = yandex_compute_instance.web,
-    databases  = yandex_compute_instance.db,
-    storage  = [yandex_compute_instance.storage]
-  })
-  filename = "${abspath(path.module)}/hosts.cfg"
-}
--- src/locals.tf
+++
@@ -1,3 +0,0 @@
-ocals {
-  ssh_public_key = file("~/.ssh/id_ed25519.pub")
-}
